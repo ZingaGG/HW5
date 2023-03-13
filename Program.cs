8522ed6991dc099ed7b9a12160135ad78cc99364
ZingaG <trad_bar@mail.ru>
2023-03-13 19:27:47 +0300

    CreateArray function added

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int[] CreateArray(int Length, int Min, int Max)
+{
+    int [] array = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = Random.Shared.Next(Min, Max + 1);
+    }
+}
