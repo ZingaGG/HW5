0a61cf66571dc4043770c5c4704c0d57a908f908
ZingaG <trad_bar@mail.ru>
2023-03-13 19:36:21 +0300

    CountEvenInArray function added

--- Program.cs
+++ Program.cs
@@ -5,4 +5,20 @@
     {
         array[i] = Random.Shared.Next(Min, Max + 1);
     }
+
+    return array;
+}
+
+int CountEvenInArray(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length ; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            result++;
+        }
+    }
+
+    return result;
 }
